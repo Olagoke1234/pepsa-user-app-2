ff393b8bdad238cebb84e305fa858d8e31597189
Olagoke Olorunkosebi <olagokeolorunkosebi@gmail.com>
2025-01-15 14:58:22 +0100

    Added base path

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Ensures relative paths are used in production
   optimizeDeps: {
     include: ["react-router-dom"],
   },
@@ -20,24 +13,3 @@ export default defineConfig({
     },
   },
 });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     include: ["react-router-dom"],
-//   },
-//   build: {
-//     rollupOptions: {
-//       external: ["react-router-dom"],
-//     },
-//   },
-//   resolve: {
-//     alias: {
-//       react: require.resolve("react"),
-//       "react-dom": require.resolve("react-dom"),
-//     },
-//   },
-// });
