09a488fb221ee7cfbefeab29ae787145a10db9ac
Olagoke Olorunkosebi <olagokeolorunkosebi@gmail.com>
2025-01-15 13:45:28 +0100

    Clear wrong vite settings

--- vite.config.js
+++ vite.config.js
@@ -6,21 +6,6 @@
 //   plugins: [react()],
 // })
 
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     include: ["react-router-dom"],
-//   },
-//   build: {
-//     rollupOptions: {
-//       external: ["react-router-dom"],
-//     },
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -34,10 +19,25 @@ export default defineConfig({
       external: ["react-router-dom"],
     },
   },
-  resolve: {
-    alias: {
-      react: require.resolve("react"),
-      "react-dom": require.resolve("react-dom"),
-    },
-  },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ["react-router-dom"],
+//   },
+//   build: {
+//     rollupOptions: {
+//       external: ["react-router-dom"],
+//     },
+//   },
+//   resolve: {
+//     alias: {
+//       react: require.resolve("react"),
+//       "react-dom": require.resolve("react-dom"),
+//     },
+//   },
+// });
